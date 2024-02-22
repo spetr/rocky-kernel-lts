e85d9f6bb3e743744d52fb220c7b955c995c85a5
Stanislav Petr <petr@digitaldata.cz>
2024-02-22 12:41:28 +0000

    Add ZFS build dependencies

--- docker/build-container.sh
+++ docker/build-container.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+cd "$(dirname "$0")"
+
 docker build --platform=linux/amd64 -t rocky-kernel-lts .
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 KERNEL_VERSION="6.6.17"
+ZFS_VERSION="2.2.2"
 
 if [ ! -f /root/rpmbuild/SOURCES/linux-${KERNEL_VERSION}.tar.xz ]; then
     wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${KERNEL_VERSION}.tar.xz -O /root/rpmbuild/SOURCES/linux-${KERNEL_VERSION}.tar.xz
+fi
+
+if [ ! -f /root/rpmbuild/SOURCES/zfs-${ZFS_VERSION}.tar.gz ]; then
+    wget https://github.com/openzfs/zfs/releases/download/zfs-2.2.2/zfs-2.2.2.tar.gz -O /root/rpmbuild/SOURCES/zfs-${ZFS_VERSION}.tar.gz
 fi
 
 rpmbuild -ba /root/rpmbuild/SPECS/kernel-lts-6.6.spec
