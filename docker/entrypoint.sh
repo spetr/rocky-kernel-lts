7083f41d450639b29394dc6b063a22a850b0fe04
Stanislav Petr <petr@digitaldata.cz>
2024-02-22 13:11:50 +0000

    Build ZFS

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,4 +11,19 @@ if [ ! -f /root/rpmbuild/SOURCES/zfs-${ZFS_VERSION}.tar.gz ]; then
     wget https://github.com/openzfs/zfs/releases/download/zfs-2.2.2/zfs-2.2.2.tar.gz -O /root/rpmbuild/SOURCES/zfs-${ZFS_VERSION}.tar.gz
 fi
 
+# Build kernel
+cd /root
 rpmbuild -ba /root/rpmbuild/SPECS/kernel-lts-6.6.spec
+
+# Build ZFS
+cd /root
+rpm -i /root/rpmbuild/RPMS/x86_64/kernel-lts-devel-${KERNEL_VERSION}-1.el9.x86_64.rpm
+tar -xf /root/rpmbuild/SOURCES/zfs-${ZFS_VERSION}.tar.gz -C /tmp
+cd /tmp/zfs-${ZFS_VERSION}
+./configure
+make srpm-utils srpm-kmod
+rpm -i zfs-2.2.2-1.el9.src.rpm zfs-kmod-2.2.2-1.el9.src.rpm
+
+cd /root
+rpmbuild -ba /root/rpmbuild/SPECS/zfs.spec
+rpmbuild -ba /root/rpmbuild/SPECS/zfs-kmod.spec
